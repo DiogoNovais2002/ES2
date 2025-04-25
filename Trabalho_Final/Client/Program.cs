0ecabeff675c685b7499153bfa4fa476953d8c31
joaocarlos994 <joaocarlosralha@ipvc.pt>
2025-04-25 19:08:48 +0100

    teste

--- Trabalho_Final/Client/Program.cs
+++ Trabalho_Final/Client/Program.cs
@@ -2,16 +2,21 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
 using Client.Services;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// HttpClient para sua API
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5140/") });
 
-
-
-// Regista o ApiService como injetável
+// seu ApiService existente
 builder.Services.AddScoped<ApiService>();
+
+// registra o UserService
+builder.Services.AddScoped<UserService>();
 
 await builder.Build().RunAsync();
